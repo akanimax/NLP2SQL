46012fe6983c08b6c05b7b180eefbfeb06d83c46
akanimax <animesh.karnewar@mobiliya.com>
2017-10-04 10:05:53 +0530

    minor modifications

--- sample_quepy_project/sample_quepy_project/basic.py
+++ sample_quepy_project/sample_quepy_project/basic.py
@@ -6,7 +6,7 @@ Basic queries for sample_quepy_project quepy.
 This module simply presents a semantic representation of the input natural language sentences
 """
 
-# regex stuff required for nlp. 
+# regex stuff required for nlp.
 from refo import Group, Question
 from quepy.dsl import HasKeyword
 from quepy.parsing import Lemma, Pos, QuestionTemplate
@@ -25,9 +25,9 @@ class WhatIs(QuestionTemplate):
     regex = Lemma("what") + Lemma("be") + target + Question(Pos("."))
 
     def interpret(self, match):
-	""" Define the interpret method by using the dsl module to convert this into a sql query. """
+    	""" Define the interpret method by using the dsl module to convert this into a sql query. """
 
-	thing = match.target.tokens
-	target = HasKeyword(thing)
-	definition = IsDefinedIn(target)
-	return definition
+    	thing = match.target.tokens
+    	target = HasKeyword(thing)
+    	definition = IsDefinedIn(target)
+    	return definition
